3897947df98167f1ca50fc81491a2bda861233d9
HP <HP@LAPTOP-MRE5INU1>
2021-03-17 16:25:56 +0530

    commiting sample1 to L

---
+++ src/com/cg/trg/pi/sample1.java
@@ -0,0 +1,24 @@
+package com.cg.trg.pi;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+
+
+				int n=10;
+				if(n>1) {
+					System.out.println("1");
+				}
+				else {
+					System.out.println("0");
+				}
+						
+
+			}
+
+		}
+
+
+	
+
+
